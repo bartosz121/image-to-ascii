b2399dd41e5a32a9e057b1d67b7afb51736b0d9e
bartosz121 <bmagiera121@gmail.com>
2021-09-26 22:49:15 +0200

    Add `open_image` `img_to_ascii` and `main` functions
    - `open_image` tries to open image
    - `img_to_ascii` does the actual work
    - `main` wraps it up

--- image_2_ascii/i2a.py
+++ image_2_ascii/i2a.py
@@ -1,28 +1,40 @@
+import sys
 from numpy import asarray
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 gs_to_char = list(".^,:;Il!i><~+_-?][{}1)(\\/tfjrxnuvczXYUCLQ0OZmwqpdbkhao*#MW&8%B@$")
 
 
-def transform(file):
-    with open("ascii.txt", "w") as f:
-        with Image.open(file) as img:
-            # resize the image
-            aspect_ratio = img.width / img.height
-            print(
-                "=================\n" "image loaded\n" f"size: {img.width}x{img.height}"
-            )
-            img = img.resize((200, int(img.height * 0.22 * aspect_ratio)))
-            print(f"resized to: {img.width}x{img.height}")
-            data = asarray(img.convert("L"))
-            for y in data:
-                for x in y:
-                    f.write(gs_to_char[x // 4])
-                f.write("\n")
+def open_image(img_path: str):
+    try:
+        img = Image.open(img_path)
+    except (FileNotFoundError, UnidentifiedImageError, ValueError, TypeError) as err:
+        print(err)
+        sys.exit(1)
+    return img
 
-            f.close()
-            print("done\n=================")
+
+def img_to_ascii(img: Image):
+    with open("ascii.txt", "w") as f:
+        # resize the image
+        aspect_ratio = img.width / img.height
+        print("=================\n" "image loaded\n" f"size: {img.width}x{img.height}")
+        img = img.resize((200, int(img.height * 0.22 * aspect_ratio)))
+        print(f"resized to: {img.width}x{img.height}")
+        data = asarray(img.convert("L"))
+        for y in data:
+            for x in y:
+                f.write(gs_to_char[x // 4])
+            f.write("\n")
+
+        f.close()
+        print("done\n=================")
+
+
+def main(file: str):
+    img = open_image(file)
+    img_to_ascii(img)
 
 
 if __name__ == "__main__":
-    transform("lenna.png")
+    main("/home/bartosz/code/image-2-ascii/image_2_asci/lenna.png")
